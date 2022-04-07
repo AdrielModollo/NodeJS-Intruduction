01c9d7df6adbd1a2c1196011d6e78b3efc0a4f92
Adriel.Modollo <adriel.modollo@saudeid.com.br>
2022-04-07 16:31:17 -0300

    Ouvindo eventos uma única vez

--- Events/events.js
+++ Events/events.js
@@ -1,8 +1,10 @@
 const { EventEmitter } = require('events')
 const ev = new EventEmitter()
 
-ev.on('saySomething', (message) => {
+ev.once('saySomething', (message) => {
     console.log('Eu ouvi você', message)
-}) //ouvindo
+}) //once ouve somente uma única vez
 
 ev.emit('saySomething', "Adriel")
+ev.emit('saySomething', "Marina")
+ev.emit('saySomething', "Luna")
