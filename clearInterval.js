7a320ef4aa66baa3a69f086dfb3e51a230096fe9
Adriel.Modollo <adriel.modollo@saudeid.com.br>
2022-04-07 16:22:07 -0300

    clearInterval

---
+++ clearInterval.js
@@ -0,0 +1,8 @@
+// clearInterval irá cancelar um setInterval registrado
+
+const timeOut = 1000
+const checking = () => console.log('checking!')
+
+let interval = setInterval(checking, timeOut)
+
+setTimeout( () => clearInterval(interval), 3000) // Cancela após 3 segundos
