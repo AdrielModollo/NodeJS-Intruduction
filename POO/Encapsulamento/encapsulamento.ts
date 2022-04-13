60f17ece9be0e05f6af04b8bfedf1b188350185c
AdrielModollo <adrielmodollo@outlook.com>
2022-04-12 21:12:06 -0300

    Encapsulamento POO e Estrutural

--- POO/Encapsulamento/encapsulamento.ts
+++ POO/Encapsulamento/encapsulamento.ts
@@ -1,1 +1,31 @@
-console.log("teste 4 contabilização de commits")
+//Orientado a Objetos   
+
+class Poligono {
+    constructor(altura, largura) {
+        this.altura = altura
+        this.largura = largura
+    }
+
+    get area() {
+        return this.#calcularArea()
+    }
+
+    #calcularArea() {
+        return this.altura * this.largura
+    }
+}
+
+let poligono = new Poligono(50, 60)
+console.log(poligono.area)
+
+/*Estrutural
+
+let altura = 50
+let largura = 60
+
+function calcularArea() {
+    return altura * largura
+}
+
+let area = calcularArea()
+*/
