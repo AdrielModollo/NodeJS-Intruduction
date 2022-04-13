143eacec240b4f4c0e0097202966259bbaf34ea4
AdrielModollo <adrielmodollo@outlook.com>
2022-04-12 21:57:16 -0300

    POO

--- Timers/clearInterval.ts
+++ Timers/clearInterval.ts
@@ -5,4 +5,4 @@ const checking = () => console.log('checking!')
 
 let interval = setInterval(checking, timeOut)
 
-setTimeout( () => clearInterval(interval), 3000) // Cancela após 3 segundos
+setTimeout(() => clearInterval(interval), 3000) // Cancela após 3 segundos
